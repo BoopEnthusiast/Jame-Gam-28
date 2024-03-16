b009207e6634959746b3b977a45077b2092943f5
Sheep26 <crabeykiller@gmail.com>
2024-03-16 19:00:21 +1300

    flower speed

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -5,7 +5,7 @@ var health: float = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_grow(0.0009 * delta)
+	_grow(0.09 * delta)
 
 func _grow(amount: float):
 	scale.y += amount
