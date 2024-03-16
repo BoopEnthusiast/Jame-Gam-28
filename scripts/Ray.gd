1a404be9ba31de96473a102bb27b343b71fbf78f
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-03-17 01:05:14 +1300

    Spawning different towers

--- scripts/Ray.gd
+++ scripts/Ray.gd
@@ -4,13 +4,19 @@ extends RayCast3D
 var ground_node: StaticBody3D
 @onready var player := $".."
 @onready var tower_limiter := $"../../TowerLimiter"
+@onready var tower_menu := $"../../UI/TowerSpawnMenu"
 
 # Preload scenes to spawn
 var test_tower = preload("res://scenes/tower_base.tscn")
 
+#variables
+var spawn_location: Vector3
+
+
 # Get the ground node
 func _ready() -> void:
 	ground_node = get_node("../../../Ground")
+
 
 # Interact with objects
 func _physics_process(_delta) -> void:
@@ -22,8 +28,6 @@ func _physics_process(_delta) -> void:
 			for body in nearby_bodies:
 				if body is Tower:
 					return
-			
-			#var new_tower: StaticBody3D = test_tower.instantiate()
-			#ground_node.add_child(new_tower)
-			#new_tower.global_position = get_collision_point()
-			#new_tower.rotation.y = player.rotation.y
+			spawn_location = get_collision_point()
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			tower_menu.visible = true
