a0e20d856bf16e9384411fd685381c17d58018d5
Sheep26 <crabeykiller@gmail.com>
2024-03-16 18:59:54 +1300

    flower

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -1,6 +1,17 @@
 extends StaticBody3D
 
+@onready var head = $Head
+var health: float = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	scale.y += 9 * delta
+	_grow(0.0009 * delta)
+
+func _grow(amount: float):
+	scale.y += amount
+
+func _damage(dp: float):
+	health -= dp
+
+func _get_health() -> float:
+	return health
