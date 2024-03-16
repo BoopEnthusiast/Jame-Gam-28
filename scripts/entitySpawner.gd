13f9000869dcff598eb72bc4524dcc02b4801c4c
Sheep26 <crabeykiller@gmail.com>
2024-03-16 20:42:11 +1300

    entity spawn delay

--- scripts/entitySpawner.gd
+++ scripts/entitySpawner.gd
@@ -1,11 +1,20 @@
 extends Node
 
 @onready var testEnemy = preload("res://scenes/testEnemy.tscn")
+@export var SPAWN_DELAY_SECONDS: int
+var time
 
-func _process(_delta):
-	# Spawn the entity
-	var instance = testEnemy.instantiate()
-	instance.position.x = randf_range(-30, 30)
-	instance.position.y = 5
-	instance.position.z = randf_range(-30, 30)
-	get_tree().root.add_child(instance)
+func _ready():
+	time = SPAWN_DELAY_SECONDS
+
+func _process(delta):
+	# Delay for the spawns
+	if time <= 0:
+		time = SPAWN_DELAY_SECONDS
+		# Spawn the entity
+		var instance = testEnemy.instantiate()
+		instance.position.x = randf_range(-30, 30)
+		instance.position.y = 5
+		instance.position.z = randf_range(-30, 30)
+		get_tree().root.add_child(instance)
+	else: time -= delta
