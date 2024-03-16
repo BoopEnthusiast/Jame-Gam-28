cc3bc7c47d3371a697c2720ed2d6b952595e3422
Boop <onpotis@gmail.com>
2024-03-16 18:56:00 +1300

    toon shaders

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -1,15 +1,6 @@
 extends StaticBody3D
 
-@onready var head = $Head
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	scale = Vector3(1, 1, 1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(delta)
-	_grow(9 * delta)
-
-func _grow(amount: float):
-	scale.y += amount
+	scale.y += 9 * delta
