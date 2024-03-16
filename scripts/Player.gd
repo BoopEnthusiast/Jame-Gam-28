d335d5a482657d81ae77ae57fd050d36366f425e
Boop <onpotis@gmail.com>
2024-03-16 14:57:31 +1300

    change the jump to feel more bouncy

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const RUN_SPEED = 8.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 5.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
