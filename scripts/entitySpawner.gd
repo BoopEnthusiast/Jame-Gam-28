c8283becbec981d9ede054845e4c4fcebb43f5a7
Sheep26 <crabeykiller@gmail.com>
2024-03-16 20:21:16 +1300

    e

--- scripts/entitySpawner.gd
+++ scripts/entitySpawner.gd
@@ -3,12 +3,9 @@ extends Node
 @onready var testEnemy = preload("res://scenes/testEnemy.tscn")
 
 func _process(_delta):
-	# Wait to spawn entity
-	await get_tree().create_timer(5).timeout
-	
 	# Spawn the entity
 	var instance = testEnemy.instantiate()
 	instance.position.x = randf_range(-30, 30)
 	instance.position.y = 5
 	instance.position.z = randf_range(-30, 30)
-	#get_tree().root.add_child(instance)
+	get_tree().root.add_child(instance)
