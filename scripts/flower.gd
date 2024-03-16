f63b229b44d88aec468a3fa90b77c22b70c34c41
Boop <onpotis@gmail.com>
2024-03-16 18:34:04 +1300

    fixing flower

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -5,11 +5,11 @@ extends StaticBody3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	scale = Vector3(1, 1, 1)
-	head.position = Vector3(0, 1, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_grow(0.00009 * delta)
+	print(delta)
+	_grow(9 * delta)
 
 func _grow(amount: float):
 	scale.y += amount
