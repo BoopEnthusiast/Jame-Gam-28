f88a6ff69f9b23cf25bd66efc59737a2a4560bab
Boop <onpotis@gmail.com>
2024-03-17 15:07:17 +1300

    push

--- scripts/singleton.gd
+++ scripts/singleton.gd
@@ -5,9 +5,14 @@ var flower_health: int = 100
 var growth: float = 0
 var water: int = 100
 
-
 func _process(delta) -> void:
 	growth += 1 * delta
 
 func get_flower_health() -> int:
 	return flower_health
+
+func reset():
+	player_health = 100
+	flower_health = 100
+	growth = 0
+	water = 100
