53e7cbecd752f583a8992b37de98abdd806cfef0
Boop <onpotis@gmail.com>
2024-03-17 14:54:14 +1300

    spuirting cucumber

--- scripts/squirting.gd
+++ scripts/squirting.gd
@@ -1,5 +1,10 @@
 extends "res://scripts/tower.gd"
 
+@onready var head = $Head
+@onready var cooldown: Timer = $FireCooldown
 
-func _physics_process(delta):
-	pass
+func _on_range_body_entered(body):
+	if body is enemy and cooldown.is_stopped():
+		head.look_at_from_position(body.position, Vector3.UP)
+		body.call("damage", damage_points)
+		cooldown.start()
